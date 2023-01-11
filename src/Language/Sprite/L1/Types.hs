4f50bda8a054aa044585286b5025d2c449f6b8cc
Kartik Singhal <kartiksinghal@gmail.com>
2023-01-11 12:39:30 -0600

    Cleanup L1

--- src/Language/Sprite/L1/Types.hs
+++ src/Language/Sprite/L1/Types.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveFunctor     #-}
 
-module Language.Sprite.L1.Types where 
+module Language.Sprite.L1.Types where
 
 import qualified Language.Fixpoint.Horn.Types  as H
 import qualified Language.Fixpoint.Types       as F
-import qualified Language.Sprite.Common.UX      as UX
-import           Language.Sprite.Common.Misc    
+import qualified Language.Sprite.Common.UX     as UX
+import           Language.Sprite.Common.Misc
 import           Language.Sprite.Common
 
 -- | Basic types --------------------------------------------------------------
@@ -16,38 +16,35 @@ data Base = TInt
 
 -- | Refined Types ------------------------------------------------------------
 
-data Type r 
-  = TBase !Base r                               -- Int{r} 
-  | TFun  !F.Symbol !(Type r) !(Type r)         -- x:s -> t 
+data Type r
+  = TBase !Base r                               -- Int{r}
+  | TFun  !F.Symbol !(Type r) !(Type r)         -- x:s -> t
   deriving (Eq, Ord, Show)
 
 type RType = Type F.Reft
 
 -- | Primitive Constants ------------------------------------------------------
 
-data PrimOp 
-  = BPlus 
-  | BMinus 
+data PrimOp
+  = BPlus
+  | BMinus
   | BTimes
-  | BLt
-  | BGt
-  | BEq 
   deriving (Eq, Ord, Show)
 
-data Prim 
+data Prim
   = PInt Integer
-  | PBin !PrimOp 
+  | PBin !PrimOp
   deriving (Eq, Ord, Show)
 
 -- | Terms --------------------------------------------------------------------
 data Decl a = Decl (Bind a) (Expr a)  a
   deriving (Show, Functor)
 
-data Bind a 
+data Bind a
   = Bind !F.Symbol a
   deriving (Eq, Ord, Show, Functor)
 
-bindId :: Bind a -> F.Symbol 
+bindId :: Bind a -> F.Symbol
 bindId (Bind x _) = x
 
 data Imm a
@@ -55,7 +52,7 @@ data Imm a
   | ECon !Prim     a
   deriving (Show, Functor)
 
-data Expr a 
+data Expr a
   = EImm !(Imm  a)            a
   | EFun !(Bind a) !(Expr a)  a
   | EApp !(Expr a) !(Imm  a)  a
@@ -63,28 +60,28 @@ data Expr a
   | EAnn !(Expr a) !RType     a
   deriving (Show, Functor)
 
-instance Label Imm  where 
+instance Label Imm  where
   label (EVar _ l) = l
   label (ECon _ l) = l
 
-instance Label Expr where 
+instance Label Expr where
   label (EImm _   l) = l
   label (EFun _ _ l) = l
   label (EApp _ _ l) = l
   label (ELet _ _ l) = l
   label (EAnn _ _ l) = l
- 
-instance Label Decl where 
+
+instance Label Decl where
   label (Decl _ _ l) = l
 
 ------------------------------------------------------------------------------
 declsExpr :: [Decl a] -> Expr a
 ------------------------------------------------------------------------------
-declsExpr [d]    = ELet d (intExpr 0 l)  l where l = label d 
-declsExpr (d:ds) = ELet d (declsExpr ds) l where l = label d 
-declsExpr _      = UX.panic "declsExpr with empty declarations!" UX.junkSpan  
-  
-intExpr :: Integer -> a -> Expr a 
+declsExpr [d]    = ELet d (intExpr 0 l)  l where l = label d
+declsExpr (d:ds) = ELet d (declsExpr ds) l where l = label d
+declsExpr _      = UX.panic "declsExpr with empty declarations!" UX.junkSpan
+
+intExpr :: Integer -> a -> Expr a
 intExpr i l = EImm (ECon (PInt i) l) l
 
 ------------------------------------------------------------------------------
@@ -93,12 +90,12 @@ type SrcBind   = Bind F.SrcSpan
 type SrcDecl   = Decl F.SrcSpan
 type SrcExpr   = Expr F.SrcSpan
 
-instance F.Subable r => F.Subable (Type r) where 
-  -- syms   :: a -> [Symbol]                 
+instance F.Subable r => F.Subable (Type r) where
+  -- syms   :: a -> [Symbol]
   syms (TBase _ r)  = F.syms r
   syms (TFun _ s t) = F.syms s ++ F.syms t
 
-  -- substa :: (Symbol -> Symbol) -> Type r -> Type r 
+  -- substa :: (Symbol -> Symbol) -> Type r -> Type r
   substa f (TBase b r)  = TBase b (F.substa f r)
   substa f (TFun x s t) = TFun x  (F.substa f s) (F.substa f t)
 
@@ -109,5 +106,3 @@ instance F.Subable r => F.Subable (Type r) where
   -- subst  :: Subst -> a -> a
   subst f (TBase b r)  = TBase b (F.subst f r)
   subst f (TFun x s t) = TFun  x (F.subst f s) (F.subst f t)
-
-
